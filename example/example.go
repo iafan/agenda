adea6c9909a43e1c2e60dab493e980f00b1f1efd
Igor Afanasyev <igor.afanasyev@gmail.com>
2017-05-23 11:20:28 -0700

    Make Go Report Card happy

--- example/example.go
+++ example/example.go
@@ -2,14 +2,17 @@ package example
 
 import "errors"
 
+// Sum calculates a sum of two integers
 func Sum(a int, b int) int {
 	return a + b
 }
 
+// Mul multiplies two integers
 func Mul(a int, b int) int {
 	return a * b
 }
 
+// Div performs an integer division of two integers
 func Div(a int, b int) (int, error) {
 	if b == 0 {
 		return 0, errors.New("Division by zero")
